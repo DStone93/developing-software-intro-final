57b059f0fc762fb2bf97aa600fee3fe55d16ee81
Derrick <dstone16640@alumni.focuscollege.com>
2020-10-03 18:58:58 -0700

    Made two setup commands, started working on Softwareinstructions.md

---
+++ SoftwareInstructions.md
@@ -0,0 +1,36 @@
+# Software Instructions
+
+![SoftwareInstructions]
+---
+Created and checked out branch feature/yargs
+Installed yargs using:
+  +  npm i @types/yargs --save-dev
+
+Setup two commands in a new folder within the src folder
+  + calc-house-materials
+ ~~[Read More](./src/commands/calc-house-materials.ts)~~
+  + get-house-materials
+~~[Read More](./src/commands/calc-house-materials.ts)~~
+  + // Requirements are to setup two commands for further coding 
+
+Versioned project to 1.21.0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+[SoftwareInstructions]: https://rapidapi.com/blog/wp-content/uploads/2019/07/background-bit-bytes-2004161-768x512.jpg
+
---
+++ src/commands/calc-house-materials.ts
@@ -0,0 +1,64 @@
+import { Arguments, Argv } from "yargs";
+
+export function calcHouseMaterials(yargs: Argv): void {
+    // create a new yargs "command"
+    yargs.command(
+        // name the command with no spaces
+        "calc-house-materials",
+
+        // describe the command so that the --help flag is helpful
+        "Calculate the required lumber to build a house with given dimensions",
+
+        // define the parameters we need for our command
+        {
+            width: {
+                type: "number",
+                alias: "w",
+                description: "The width of the house",
+            },
+
+            length: {
+                type: "number",
+                alias: "l",
+                description: "The length of the house",
+            },
+
+            isFeet: {
+                type: "boolean",
+                alias: "f",
+                description: "The flag to toggle input as feet",
+            },
+
+            name: {
+                type: "string",
+                alias: "client",
+                description: "Name of the customer",
+            },
+        },
+        // define the function we want to run once the arguments are parsed
+        function (
+            args: Arguments<{
+                name: string;
+                width: number;
+                length: number;
+                isFeet: boolean;
+                client: string;
+                w: number;
+                l: number;
+                f: boolean;
+            }>
+        ) {
+            // As per step 1: Yargs
+            // Create two commands (1/2) for further coding
+            // Each command should use console.log to output the parameters passed
+            const requirements = YetTobeDetermined {
+                args.name,
+                args.width,
+                args.length,
+                args.isFeet
+            );
+
+            console.log(requirements);
+        }
+    );
+}
