b80a29e485e168532fec46a8e36fb795247a9f49
Derrick <dstone16640@alumni.focuscollege.com>
2020-10-08 21:03:08 -0700

    Playing around with code, exploring TS

--- src/calculator/index.ts
+++ src/calculator/index.ts
@@ -1,18 +1,23 @@
-import { IHouseDimensions} from "./interfaces"
+import { IHouseDimensions } from "./interfaces";
 
 // Simple function to be called from calc-house-materials in src/commands
 export function calcHouseMaterials(
     name: string,
     width: number,
     length: number,
-    units: boolean
-) {
-    return { name, width, length, units }
+    units?: boolean
+) /*:IHouseDimensions*/ {
+    return { name, width, length, units };
     /* Expected 
-    { name: 'gerald', width: 8, length: 8, units: true } 5*/
-};
+    { name: 'gerald', width: 8, length: 8, units: true } */
+}
 
 export function getHouseMaterials(name: string) {
     return name;
     // Expected "Gerald"
-};
+}
+
+// function InchesTofeet (number:number){
+//     let newNumber = number / 12;
+//     return newNumber
+// }
