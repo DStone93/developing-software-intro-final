c4cbadc55046051a31097896e880197f240f4f96
Derrick <dstone16640@alumni.focuscollege.com>
2020-10-31 21:14:50 -0700

    Working on Step 5, fixed samplers

--- src/calculator/index.ts
+++ src/calculator/index.ts
@@ -1,6 +1,17 @@
 import { HouseMaterials } from "../commands/calc-house-materials";
 import { IHouseDimensions } from "./interfaces";
 
+
+const BEAM_WIDTH = 3.5;
+const BOARD_LENGTH = 8 * 12;
+const STUDS_OFFSET = 16;
+const BEAMS_REQUIRED_EVERY_INCHES = 20 * 12;
+
+
+function feetToInches (feet:number){
+    return feet * 12
+}
+
 // Simple function to be called from calc-house-materials in src/commands
 export function calcHouseMaterials(
     name: string,
@@ -8,6 +19,10 @@ export function calcHouseMaterials(
     length: number,
     units?: boolean
 ): IHouseDimensions {
+    if (units == true) {
+        width = feetToInches(width);
+        length = feetToInches(length);
+      }
     return {
         name: name,
         house: {
@@ -106,7 +121,58 @@ export function getHouseMaterials(name: string): IHouseDimensions {
 
 }
 
-// function InchesTofeet (number:number){
-//     let newNumber = number / 12;
-//     return newNumber
-// }
+function getPlatesInLength(inches: number) {
+    // devide the length by 96 inches (8 feet) and round up
+    // multiply by three because we're doing the top and bottom in one calculation
+    return Math.ceil(inches / BOARD_LENGTH) * 3;
+}
+
+function getStudsInLength(inches: number) {
+    // calculate the studs across
+    // round up to account for the last one
+    const studs = Math.ceil(inches / STUDS_OFFSET);
+
+    // make sure we add an end piece if we have a perfect multiple of 16
+    const isNotPerfectWidth = Math.min(inches % STUDS_OFFSET, 1);
+    const perfectWidthExtension = isNotPerfectWidth * -1 + 1;
+    return studs + perfectWidthExtension;
+}
+
+function getBoardsInLength(inches: number): number {
+    const plates = getPlatesInLength(inches);
+    const studs = getStudsInLength(inches);
+
+    return plates + studs;
+}
+
+function getRequiredBeamsInLength(inches: number) {
+    // for every 20 feet, we need one beam
+    // we know our wall is at least 20 feet, so calculate the required beams for the REST of the wall
+    // if our wall is under 20 feet, this will return zero
+    const wallLengthOverMinRequired = getWallLengthOverMinimumRequiredBeforeBeam(
+        inches
+    );
+    const wallLengthPlusBeam = BEAMS_REQUIRED_EVERY_INCHES + BEAM_WIDTH;
+    const requiredBeams = Math.ceil(
+        wallLengthOverMinRequired / wallLengthPlusBeam
+    );
+
+    return requiredBeams;
+}
+
+function getWallLengthOverMinimumRequiredBeforeBeam(inches: number): number {
+    return Math.max(inches - BEAMS_REQUIRED_EVERY_INCHES, 0);
+}
+
+export function calcWallLumber (inches:number) {
+    //In the order of what the function will return
+    const plates = getPlatesInLength(inches)
+    const studs = getStudsInLength(inches)
+    const posts = getRequiredBeamsInLength(inches)
+
+    return {
+        plates:plates,
+        studs:studs,
+        posts: posts
+    };
+}
--- tests/default.spec.ts
+++ tests/default.spec.ts
@@ -1,45 +1,47 @@
 import { expect } from 'chai'
-import {calcHouseMaterials, getHouseMaterials} from '../src/calculator/index'
+import {calcHouseMaterials, getHouseMaterials, calcWallLumber} from '../src/calculator/index'
 
 import "mocha";
 
-// describe("calcHouseMaterials Function", () => {
-//   it("should return House Materials", () => {
-//       const result = calcHouseMaterials("test", 8, 8, false);
-//       expect(result.name).to.equal("test");
-//       expect(result.house.length).to.equal(8);
-//       expect(result.house.width).to.equal(8);
-//   });
-// });
 
-// describe ("calcHouseMaterials"), () => {
-//   it("Should return the name gerald"), () => {
-//     const result = calcHouseMaterials("gerald", 8, 8, false);
-//     expect(result.name).to.equal("gerald");
- 
-//   });
-// });
+// Changed calcHouseMaterials to multiply by 12 if flag is present
 
-describe("Creates a house object as defined in IHouse", () => {
+describe("calcHouseMaterials", () => {
   it("should return customer name gerald", () => {
       const result= calcHouseMaterials("gerald", 8, 8, false)
       expect(result.name).to.equal("gerald")
   });
   
-  it("should return length as 8", () => {
+  it("should return length as 96", () => {
       const result = calcHouseMaterials("gerald", 8, 8, false)
       expect(result.house.length).to.equal(8);
   });
   
-  it("should return width as 8", () => {
+  it("should return width as 96", () => {
       const result= calcHouseMaterials("gerald", 8, 8, false)
       expect(result.house.width).to.equal(8);
   });
 }); 
 
-describe("getHouseMaterials Function", () => {
+describe("getHouseMaterials", () => {
   it("should return House Name", () => {
       const result = getHouseMaterials("gerald");
       expect(result.name).to.equal("gerald");
   });
 }); 
+
+
+// // Tests for calcWallLumber
+// describe ("calcWallLumber"), () => {
+//   it("Hopefully returns plates, studs, and posts", () => {
+//     const result = calcWallLumber(0);
+//     expect(result.plates).to.equal(0);
+// });
+// });
+
+// // describe ("calcWallLumber"), () => {
+// //   it("Hopefully returns plates, studs, and posts", () => {
+// //     const result = calcWallLumber(0);
+// //     expect(result.plates).to.equal(0)
+// //   });
+// // });
